0fd88552ed40a5538d18ab1f0297675995d0a392
Stephan Lipp <sphlpp91@gmail.com>
2025-07-22 11:25:24 +0200

    refactor: remove unused imports

--- src/fuzztastic/visualization/treemap.py
+++ src/fuzztastic/visualization/treemap.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import random
-import time
 from collections import defaultdict, namedtuple
 from concurrent.futures import ThreadPoolExecutor
 from itertools import chain
